7ad22a943318e8397cf66c5fb9d42b3d651eeeab
UGGINA TARUN PAVAN KUMAR <98165814+Ugginagithub@users.noreply.github.com>
2026-06-25 12:53:29 +0530

    InstaFilterApp
    InstaFilterApp

---
+++ InstaFilterApp/InstaFilterApp/InstaFilterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterAppApp.swift
+//  InstaFilterApp
+//
+//  Created by Tarun on 25/06/26.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
